91a6fc0be6ed026cde3060c9002cbe19039a653e
Cheng Chi (@joychester) <joychester@gmail.com>
2014-07-07 22:31:52 +0800

    Update linesofcode.rb
    refactoring the code by combining all the folder types processing method to make it shorter.

--- linesofcode.rb
+++ linesofcode.rb
@@ -23,27 +23,15 @@ Net::SSH.start(C3_SSH_HOST,C3_SSH_Loginname,:password => C3_SSH_PWD) do |ssh|
 	
 end
 
-result_dev.each_line  do |row| 
+def post_line_of_code(folder_type)
+	folder_type.each_line  do |row| 
 		temp = row.split('=>')
 		case temp[0]
-			when '/home/cchi/mycorp/source/depot/main/reorganized_code'					   
+			when '/home/cchi/mycorp/source/depot/main/reorganized_code'
 				repo = 'depot_main'
-			when '/home/cchi/mycorp/source/mycorp/domain'					   
+			when '/home/cchi/mycorp/source/mycorp/domain'
 				repo = 'mycorp_domain'
-			else
-		end
-		lines = temp[1].chomp.to_i
-
-		p "HTTPClient POST method start"
-		post_body = { :tag => repo, :path => temp[0], :lines => lines, :cdate => currentDateStr}
-		client.post(QADash_URL, post_body)
-		p "HTTPClient POST method end!"
-end
-
-result_qe.each_line  do |row|
-		temp = row.split('=>')
-		case temp[0]
-			when '/home/cchi/mycorp/source/mycorp/qe'					   
+			when '/home/cchi/mycorp/source/mycorp/qe'
 				repo = 'mycorp_qe'
 			else
 		end
@@ -53,4 +41,8 @@ result_qe.each_line  do |row|
 		post_body = { :tag => repo, :path => temp[0], :lines => lines, :cdate => currentDateStr}
 		client.post(QADash_URL, post_body)
 		p "HTTPClient POST method end!"
+	end
 end
+
+post_line_of_code(result_dev)
+post_line_of_code(result_qe)
